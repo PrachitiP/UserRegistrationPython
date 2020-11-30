00784987b0551d5e890ccc9f6f0ee0e52ef9d9bb
PrachitiP <patilprachiti35@gmail.com>
2020-11-30 22:22:24 +0530

    Validate pattern for Email id

--- UserRegistration.py
+++ UserRegistration.py
@@ -1,1 +1,35 @@
-print("Welcome to User Registration")
+import re
+class UserRegistration:
+
+    def validateFirstName(self):
+        patternforFirstname = '^[A-Z]{1}[a-z]{3,}$'
+        if re.search(patternforFirstname, firstName):
+            return 'valid'
+        else:
+            return 'invalid'
+
+    def validateLastName(self):
+        patternforLastname = '^[A-Z]{1}[a-z]{3,}$'
+        if re.search(patternforLastname, firstName):
+            return 'valid'
+        else:
+            return 'invalid'
+
+    def validateEmail(self):
+        patternforEmail = "^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$"
+        if re.search(patternforEmail, email):
+            return 'valid'
+        else:
+            return 'invalid'
+if __name__ == '__main__':
+    print("Welcome to User Registration")
+    userreg = UserRegistration()
+    firstName = input("Enter first name:")
+    print(userreg.validateFirstName())
+    lastName = input("Enter last name")
+    print(userreg.validateLastName())
+    email = input("Enter email id")
+    print(userreg.validateEmail())
+
+
+
