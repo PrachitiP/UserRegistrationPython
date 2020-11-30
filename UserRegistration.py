f2663679b6877d49ef474c99f33ee24844b13654
PrachitiP <patilprachiti35@gmail.com>
2020-11-30 21:20:33 +0530

    Validate pattern for first name

--- UserRegistration.py
+++ UserRegistration.py
@@ -1,1 +1,15 @@
-print("Welcome to User Registration")
+import re
+class UserRegistration:
+
+    def validateFirstName(self):
+        patterforFirstname = '^[A-Z]{1}[a-z]{3,}$'
+        if re.search(patterforFirstname, firstName):
+            return 'valid'
+        else:
+            return 'invalid'
+
+if __name__ == '__main__':
+    print("Welcome to User Registration")
+    firstName = input("Enter first name:")
+    userreg = UserRegistration()
+    print(userreg.validateFirstName())
