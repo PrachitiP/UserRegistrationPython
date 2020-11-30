7d6c955742a60070914a62ad26166c0ed81e42a4
PrachitiP <patilprachiti35@gmail.com>
2020-11-30 21:46:26 +0530

    Validate pattern for Last Name

--- UserRegistration.py
+++ UserRegistration.py
@@ -8,8 +8,18 @@ class UserRegistration:
         else:
             return 'invalid'
 
+    def validateLastName(self):
+        patterforLastname = '^[A-Z]{1}[a-z]{3,}$'
+        if re.search(patterforLastname, firstName):
+            return 'valid'
+        else:
+            return 'invalid'
+
+
 if __name__ == '__main__':
     print("Welcome to User Registration")
     firstName = input("Enter first name:")
+    lastName = input("Enter last name")
     userreg = UserRegistration()
     print(userreg.validateFirstName())
+    print(userreg.validateLastName())
