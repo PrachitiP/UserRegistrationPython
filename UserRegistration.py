d409fae70514372a9c7655ba6caccbc4466ab9d9
PrachitiP <68328603+PrachitiP@users.noreply.github.com>
2020-12-01 11:41:17 +0530

    Validate pattern for Password

--- UserRegistration.py
+++ UserRegistration.py
@@ -1,20 +1,23 @@
 import re
 class UserRegistration:
-
+    
+    #UC1- Validate First Name
     def validateFirstName(self):
         patternforFirstname = '^[A-Z]{1}[a-z]{3,}$'
         if re.search(patternforFirstname, firstName):
             return 'valid'
         else:
             return 'invalid'
-
+        
+    #UC2- Validate Last Name
     def validateLastName(self):
         patternforLastname = '^[A-Z]{1}[a-z]{3,}$'
         if re.search(patternforLastname, firstName):
             return 'valid'
         else:
             return 'invalid'
-
+        
+    #UC3- Validate Pattern for Email id
     def validateEmail(self):
         patternforEmail = "^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$"
         if re.search(patternforEmail, email):
@@ -22,6 +25,7 @@ class UserRegistration:
         else:
             return 'invalid'
 
+     #UC4- Validate Phone Number
     def validatePhoneNo(self):
         patternforPhoneNo = "^[91 ]{3}[7-9]{1}[0-9]{9}$"
         if re.search(patternforPhoneNo, phoneNo):
@@ -29,6 +33,8 @@ class UserRegistration:
         else:
             return 'invalid'
 
+    
+    #UC5- Password with 8 letters
     def passwordRule1(self):
         patternforPassword1 = "^[a-zA-Z]{8}$"
         if re.search(patternforPassword1, password1):
